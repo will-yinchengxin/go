5cc5a1ec6695d69bfdb47ac24a40183a7a448b24
Will Yin <52946059+will-yinchengxin@users.noreply.github.com>
2022-09-02 16:06:10 +0800

    Update json.go

--- code/test/json/json.go
+++ code/test/json/json.go
@@ -6,6 +6,27 @@ import (
 	"time"
 )
 
+// 这里使用msgpack实现消息的序列化。messagepack是一个高效的二进制序列化协议。
+// 相比json编码后的数据的体积更小，编解码的速度更快。redis script也支持messagepack。
+func MsgpackMarshal() {
+	type Item struct {
+		Foo string
+	}
+
+	b, err := msgpack.Marshal(&Item{Foo: "bar"})
+	if err != nil {
+		panic(err)
+	}
+
+	var item Item
+	err = msgpack.Unmarshal(b, &item)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(item.Foo)
+	// Output: bar
+}
+
 func JsonUnmarshal() {
 	type FruitBasket struct {
 		Name    string    `json:"name"`
